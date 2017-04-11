42dd8067ca67f2c763fd33f0492370d57a3b2121
venkatgn <venkatadrin.g@gmail.com>
2017-04-11 08:26:54 +0530

    updated sample program

--- mymvnproj/src/test/java/yahoo/Sample.java
+++ mymvnproj/src/test/java/yahoo/Sample.java
@@ -5,5 +5,10 @@ public class Sample
   public void f1()
   {
 	  System.out.println("hello");
+	  System.out.println("new line");
+  }
+  public void f2()
+  {
+	  System.out.println("new method");
   }
 }
